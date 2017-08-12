e5f14a312a41fef8c8439c2198684676762e8e0a
DESKTOP-JVGBPF7\Sergiusz <deephans@o2.pl>
2017-08-12 19:36:57 +0200

    Global UserDetails propagation implemented

---
+++ src/pl/workreporter/web/controllers/UserDetailsController.java
@@ -0,0 +1,18 @@
+package pl.workreporter.web.controllers;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import pl.workreporter.security.login.CompleteUserDetails;
+
+/**
+ * Created by Sergiusz on 12.08.2017.
+ */
+@ControllerAdvice
+public class UserDetailsController {
+
+    @ModelAttribute("userDetails")
+    public CompleteUserDetails populateUserDetails() {
+        return (CompleteUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+}
