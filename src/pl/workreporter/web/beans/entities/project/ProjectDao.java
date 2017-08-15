41c94c98150b4f8a95065aa1a349bd046cefccdd
DESKTOP-JVGBPF7\Sergiusz <deephans@o2.pl>
2017-08-15 15:21:03 +0200

    Project dao interface added

---
+++ src/pl/workreporter/web/beans/entities/project/ProjectDao.java
@@ -0,0 +1,11 @@
+package pl.workreporter.web.beans.entities.project;
+
+import java.util.List;
+
+/**
+ * Created by Sergiusz on 15.08.2017.
+ */
+public interface ProjectDao {
+    Project getProjectById(long id);
+    List<Project> getAllProjectsInSolution(long solutionId);
+}
