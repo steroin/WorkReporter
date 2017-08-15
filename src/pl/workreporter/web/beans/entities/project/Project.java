fcec5b4bb212cd676b680c93ff539f9538e846ae
DESKTOP-JVGBPF7\Sergiusz <deephans@o2.pl>
2017-08-15 15:20:47 +0200

    Project model added

---
+++ src/pl/workreporter/web/beans/entities/project/Project.java
@@ -0,0 +1,43 @@
+package pl.workreporter.web.beans.entities.project;
+
+/**
+ * Created by Sergiusz on 15.08.2017.
+ */
+public class Project {
+    private long id;
+    private long solutionId;
+    private String name;
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getSolutionId() {
+        return solutionId;
+    }
+
+    public void setSolutionId(long solutionId) {
+        this.solutionId = solutionId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
