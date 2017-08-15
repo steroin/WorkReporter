6b23edc9834e100f113396182cee69cab24baf70
DESKTOP-JVGBPF7\Sergiusz <deephans@o2.pl>
2017-08-15 16:47:28 +0200

    Project model updated

--- src/pl/workreporter/web/beans/entities/project/Project.java
+++ src/pl/workreporter/web/beans/entities/project/Project.java
@@ -1,5 +1,7 @@
 package pl.workreporter.web.beans.entities.project;
 
+import java.util.Date;
+
 /**
  * Created by Sergiusz on 15.08.2017.
  */
@@ -8,6 +10,8 @@ public class Project {
     private long solutionId;
     private String name;
     private String description;
+    private Date creationDate;
+    private Date lastEditionDate;
 
     public long getId() {
         return id;
@@ -39,5 +43,21 @@ public class Project {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getLastEditionDate() {
+        return lastEditionDate;
+    }
+
+    public void setLastEditionDate(Date lastEditionDate) {
+        this.lastEditionDate = lastEditionDate;
     }
 }
