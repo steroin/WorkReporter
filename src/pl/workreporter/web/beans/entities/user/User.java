f268543b24683cef8c852d07c57cd63363090753
DESKTOP-JVGBPF7\Sergiusz <deephans@o2.pl>
2017-08-19 16:39:48 +0200

    User model added

---
+++ src/pl/workreporter/web/beans/entities/user/User.java
@@ -0,0 +1,151 @@
+package pl.workreporter.web.beans.entities.user;
+
+/**
+ * Created by Sergiusz on 19.08.2017.
+ */
+public class User {
+    private long id;
+    private long solutionId;
+    private long teamId;
+    private long accountId;
+    private long positionId;
+    private long personalDataId;
+    private double workingTime;
+    private String firstName;
+    private String lastName;
+    private String birthday;
+    private String phone;
+    private String login;
+    private String email;
+    private int accountStatus;
+    private String creationDate;
+    private String lastEditionDate;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getSolutionId() {
+        return solutionId;
+    }
+
+    public void setSolutionId(long solutionId) {
+        this.solutionId = solutionId;
+    }
+
+    public long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(long teamId) {
+        this.teamId = teamId;
+    }
+
+    public long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(long accountId) {
+        this.accountId = accountId;
+    }
+
+    public long getPositionId() {
+        return positionId;
+    }
+
+    public void setPositionId(long positionId) {
+        this.positionId = positionId;
+    }
+
+    public long getPersonalDataId() {
+        return personalDataId;
+    }
+
+    public void setPersonalDataId(long personalDataId) {
+        this.personalDataId = personalDataId;
+    }
+
+    public double getWorkingTime() {
+        return workingTime;
+    }
+
+    public void setWorkingTime(double workingTime) {
+        this.workingTime = workingTime;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAccountStatus() {
+        return accountStatus;
+    }
+
+    public void setAccountStatus(int accountStatus) {
+        this.accountStatus = accountStatus;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getLastEditionDate() {
+        return lastEditionDate;
+    }
+
+    public void setLastEditionDate(String lastEditionDate) {
+        this.lastEditionDate = lastEditionDate;
+    }
+}
