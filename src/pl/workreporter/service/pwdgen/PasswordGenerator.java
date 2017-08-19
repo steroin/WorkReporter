f4dc28b5558fd58cb519db4289b6e7c9cd5d48f5
DESKTOP-JVGBPF7\Sergiusz <deephans@o2.pl>
2017-08-19 18:34:40 +0200

    Added PasswordGenerator

---
+++ src/pl/workreporter/service/pwdgen/PasswordGenerator.java
@@ -0,0 +1,22 @@
+package pl.workreporter.service.pwdgen;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+/**
+ * Created by Sergiusz on 19.08.2017.
+ */
+@Service
+public class PasswordGenerator {
+    private final String alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public String generate(int length) {
+        StringBuilder pwdBuilder = new StringBuilder();
+        Random ran = new Random();
+
+        for (int i = 0; i < length; i++) {
+            pwdBuilder.append(alphabet.charAt(ran.nextInt(alphabet.length())));
+        }
+        return pwdBuilder.toString();
+    }
+}
