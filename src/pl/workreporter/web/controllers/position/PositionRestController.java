74de8d1c0a11d44405e4458dc419d2e24b0851e4
DESKTOP-JVGBPF7\Sergiusz <deephans@o2.pl>
2017-08-19 14:57:08 +0200

    Position REST controller added

---
+++ src/pl/workreporter/web/controllers/position/PositionRestController.java
@@ -0,0 +1,49 @@
+package pl.workreporter.web.controllers.position;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pl.workreporter.web.beans.entities.position.Position;
+import pl.workreporter.web.beans.entities.position.PositionDao;
+import pl.workreporter.web.beans.entities.project.Project;
+import pl.workreporter.web.beans.entities.project.ProjectDao;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+/**
+ * Created by Sergiusz on 15.08.2017.
+ */
+@RestController
+public class PositionRestController {
+    @Autowired
+    PositionDao positionDao;
+
+    @RequestMapping(value = "/solution/positions", method = GET)
+    public @ResponseBody
+    List<Position> getAllPositions(@RequestParam("id") long solutionId) {
+        List<Position> result = positionDao.getAllPositionsInSolution(solutionId);
+        return result;
+    }
+
+    @RequestMapping(value = "/solution/positions/{id}", method = DELETE)
+    public void removePosition(@RequestParam("solutionid") long solutionId, @PathVariable("id") long positionId) {
+        positionDao.removePosition(solutionId, positionId);
+    }
+
+    @RequestMapping(value = "/solution/positions", method = DELETE)
+    public void removeSelectedPositions(@RequestParam("solutionid") long solutionId, @RequestParam("positions") List<Long> positions) {
+        positionDao.removePositions(solutionId, positions);
+    }
+
+    @RequestMapping(value = "/solution/positions/{id}", method = PATCH)
+    public void updatePosition(@PathVariable("id") long positionId, @RequestBody Position position) {
+        positionDao.updatePosition(position);
+    }
+
+    @RequestMapping(value="/solution/positions", method = POST)
+    public Position addPosition(@RequestBody Map<String, String> position) {
+        return positionDao.addPosition(Long.parseLong(position.get("solutionid")), position.get("name"));
+    }
+}
